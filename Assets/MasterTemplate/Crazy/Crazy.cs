b30e0cc929b939437b5564025344edc5601b2b07
Hawker <62062830+BlckHawker@users.noreply.github.com>
2023-07-29 21:25:28 -0400

    Finished module

--- Assets/MasterTemplate/Crazy/Crazy.cs
+++ Assets/MasterTemplate/Crazy/Crazy.cs
@@ -12,32 +12,99 @@ public class Crazy : MonoBehaviour {
    public KMBombInfo Bomb;
    public KMAudio Audio;
 
-   static int ModuleIdCounter = 1;
+    [SerializeField]
+    private Material[] ratMaterials;
+
+    [SerializeField]
+    private Material[] solveMaterials;
+
+    [SerializeField]
+    private MeshRenderer surface;
+
+    [SerializeField]
+    private AudioClip audio;
+
+    static int ModuleIdCounter = 1;
    int ModuleId;
    private bool ModuleSolved;
 
-   void Awake () {
+    private bool focused = false;
+    private int startingTime;
+    private int goalTime;
+
+    private float currentTime;
+    private float audioTime;
+    private float maxAudioTime;
+    private bool playingAudio;
+    private KMAudio.KMAudioRef audioReference;
+
+   void Awake () 
+   {
       ModuleId = ModuleIdCounter++;
-      /*
-      foreach (KMSelectable object in keypad) {
-          object.OnInteract += delegate () { keypadPress(object); return false; };
-      }
-      */
-
-      //button.OnInteract += delegate () { buttonPress(); return false; };
-
+      GetComponent<KMSelectable>().OnFocus += delegate () { if (ModuleSolved) { return; } Logging("You are with the rats :D"); focused = true; };
+      GetComponent<KMSelectable>().OnDefocus += delegate () { if (ModuleSolved) { return; } Logging("You have left the rats :("); Logging($"Your total amount of time with the rats has been {FormatTime((int)currentTime)}"); focused = false; };
    }
 
-   void Start () {
-
+   void Start () 
+   {
+        surface.material = ratMaterials[Rnd.Range(0, ratMaterials.Length)];
+        startingTime = (int)Bomb.GetTime();
+        goalTime = startingTime / 5;
+        currentTime = 0;
+        Logging($"Be with the rats for {goalTime/60} minutes and {goalTime%60} seconds");
    }
 
-   void Update () {
+   void Update () 
+   {
+        if (!playingAudio && !ModuleSolved)
+        {
+            audioReference = Audio.PlaySoundAtTransformWithRef(audio.name, transform);
+            playingAudio = true;
+            maxAudioTime = audio.length;
+        }
 
+        audioTime += Time.deltaTime;
+
+        if (audioTime >= maxAudioTime)
+        {
+            playingAudio = false;
+        }
+
+        if (startingTime <= Bomb.GetTime() || !focused || ModuleSolved)
+        {
+            return;
+        }
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= goalTime)
+        {
+            Logging("The rats are appeased and will let you go");
+            surface.material = solveMaterials[Rnd.Range(0, solveMaterials.Length)];
+            playingAudio = false;
+            audioReference.StopSound();
+            GetComponent<KMBombModule>().HandlePass();
+            ModuleSolved = true;
+        }
    }
+
+    string FormatTime(int seconds)
+    {
+        return string.Format("{0:0}:{1:00}", seconds / 60, seconds % 60);
+    }
+
+    void Logging(string log)
+    {
+        if (log == "")
+        {
+            return;
+        }
+
+        Debug.LogFormat($"[Crazy? #{ModuleId}] {log}");
+    }
 
 #pragma warning disable 414
-   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
 #pragma warning restore 414
 
    IEnumerator ProcessTwitchCommand (string Command) {
